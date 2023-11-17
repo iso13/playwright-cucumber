bb551f62c7dad2023446cc4ea0915d3d72d3bb68
David Tran <davidtran@Davids-MacBook-Pro.local>
2023-11-17 08:34:42 -0600

    updated config

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,23 +1,24 @@
-import isCI from 'is-ci'
+import isCI from "is-ci";
 
 const config = {
-  paths: ['src/features/**/*.feature'],
-  requireModule: ['ts-node/register'],
-  require: ['src/steps/**/*.ts', 'src/common/**/*.ts'],
+  paths: ["src/features/**/*.feature"],
+  requireModule: ["ts-node/register"],
+  require: ["src/steps/**/*.ts", "src/common/**/*.ts"],
   format: [
-    'json:reports/cucumber-report.json',
-    'html:reports/report.html',
+    "json:reports/cucumber-report.json",
+    "html:reports/report.html"
   ],
-  formatOptions: { snippetInterface: 'async-await' },
+  formatOptions: { snippetInterface: "async-await" },
   worldParameters: {},
-  publishQuiet: true,
   retry: isCI ? 1 : 0
 };
 
+/*
 if (process.env.USE_ALLURE === 1) {
   config.format.push('./src/common/reporters/allure-reporter.ts');
 } else {
   config.format.push('@cucumber/pretty-formatter');
 }
+*/
 
 export default config;
